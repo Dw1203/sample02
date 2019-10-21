c578a719b9a4fb587088f3dffa0b7bb188e05566
文 杜 <782672174@qq.com>
2019-10-21 15:39:45 +0800

    1539

--- Controllers/ClassroomController.cs
+++ Controllers/ClassroomController.cs
@@ -85,5 +85,75 @@ namespace sample02.Controllers
             var res = _mapper.Map<List<ClassroomDetailDto>>(cls);
             return Ok(res);
         }
+
+        /// <summary>
+        /// 更新教室信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="modifyClassroom"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<IActionResult> UpdateClassroom(int id,[FromBody] ModifyClassroom modifyClassroom)
+        {
+            if(modifyClassroom==null)
+            {
+                return BadRequest();
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var cls =await _classroomService.GetTById(id);
+            if(cls==null)
+            {
+                return NotFound();
+            }
+            if(cls.Name!=modifyClassroom.Name)
+            {
+                var cls1 =await _classroomService.GetClassroomsByClassName(modifyClassroom.Name);
+                if(cls1.Count>0)
+                {
+                    return BadRequest("教室名称已存在");
+                }
+            }
+            if(cls.ClassRoomNum!=modifyClassroom.ClassRoomNum)
+            {
+                var cls2 = await _classroomService.GetClassroomsByClassNum(modifyClassroom.ClassRoomNum);
+                if(cls2.Count>0)
+                {
+                    return BadRequest("教室编号已存在");
+                }
+            }
+            cls.Name = modifyClassroom.Name;
+            cls.ClassRoomNum = modifyClassroom.ClassRoomNum;
+            cls.Address = modifyClassroom.Address;
+            _mapper.Map(cls, cls);
+            if(!await _classroomService.Save())
+            {
+                return StatusCode(500, "更新数据时出错");
+            }
+            return Created("", modifyClassroom);
+        }
+
+        /// <summary>
+        /// 删除教室信息
+        /// </summary>
+        /// <param name="id">教室id</param>
+        /// <returns></returns>
+        [HttpDelete]
+        public async Task<IActionResult> DeleteClassroom(int id)
+        {
+            var cls = await _classroomService.GetTById(id);
+            if(cls==null)
+            {
+                return NotFound();
+            }
+            _classroomService.DeleteT(cls);
+            if(!await _classroomService.Save())
+            {
+                return StatusCode(500, "删除数据时出错");
+            }
+            return NoContent();
+        }
     }
 }
